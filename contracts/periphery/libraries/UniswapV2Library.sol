296632ffa14cd32240d77e2766b5bee5e967bf26
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2022-09-27 20:55:30 -0300

    Cosmetic changes

--- contracts/periphery/libraries/UniswapV2Library.sol
+++ contracts/periphery/libraries/UniswapV2Library.sol
@@ -15,7 +15,7 @@ library UniswapV2Library {
     function pairFor(address factory, address tokenA, address tokenB) internal pure returns (address pair) {
         (address token0, address token1) = sortTokens(tokenA, tokenB);
         pair = address(uint160(uint(keccak256(abi.encodePacked(
-                hex'ff',
+                hex"ff",
                 factory,
                 keccak256(abi.encodePacked(token0, token1)),
                 hex"96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f" // init code hash
