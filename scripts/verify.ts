e797d2354e27a19c95d8cac6db66d879eb232364
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2023-02-28 00:27:16 -0300

    Fixes deploy/verify scripts

--- scripts/verify.ts
+++ scripts/verify.ts
@@ -61,7 +61,7 @@ async function main(args: string[]): Promise<void> {
 
   const PAIR = await factory.getPair(WETH, WRAPPER);
 
-  await verifyContract(ROUTER, 'contracts/periphery/UniswapV2Router01NFT.sol:UniswapV2Router01NFT', FACTORY, WETH, ADMIN, HALF_PERCENT, ONE_PERCENT);
+  await verifyContract(ROUTER, 'contracts/periphery/UniswapV2Router01NFT.sol:UniswapV2Router01NFT', FACTORY, WETH, ADMIN, ADMIN, HALF_PERCENT, ONE_PERCENT);
   await verifyContract(FACTORY, 'contracts/core/UniswapV2Factory.sol:UniswapV2Factory', ADMIN);
   await verifyContract(PAIR, 'contracts/core/UniswapV2Pair.sol:UniswapV2Pair');
   await verifyContract(WRAPPER, 'contracts/core/WERC721.sol:WERC721');
