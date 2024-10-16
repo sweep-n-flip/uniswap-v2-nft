d26355106b99b80661309c7f22c6f83c8884dcbf
Arthur RIbeiro <arthurmr13@hotmail.com>
2024-10-16 11:12:40 -0300

    feat: begin to integrate networks into the processor

--- squid/src/processor.ts
+++ squid/src/processor.ts
@@ -7,18 +7,27 @@ import {
     Log as _Log,
     Transaction as _Transaction,
 } from '@subsquid/evm-processor'
+import * as fs from 'fs';
+import * as path from 'path';
+
+const networkConfig = JSON.parse(
+    fs.readFileSync(
+        path.resolve(__dirname, `../config/${process.env.NETWORK}.json`),
+        'utf-8'
+    )
+);
 
 export const processor = new EvmBatchProcessor()
     // Lookup archive by the network name in Subsquid registry
     // See https://docs.subsquid.io/evm-indexing/supported-networks/
-    .setGateway('https://v2.archive.subsquid.io/network/ethereum-mainnet')
+    .setGateway(networkConfig.gatewayUrl)
     // Chain RPC endpoint is required for
     //  - indexing unfinalized blocks https://docs.subsquid.io/basics/unfinalized-blocks/
     //  - querying the contract state https://docs.subsquid.io/evm-indexing/query-state/
     .setRpcEndpoint({
         // Set the URL via .env for local runs or via secrets when deploying to Subsquid Cloud
         // https://docs.subsquid.io/deploy-squid/env-variables/
-        url: assertNotNull(process.env.RPC_ETH_HTTP, 'No RPC endpoint supplied'),
+        url: networkConfig.rpcUrl,
         // More RPC connection options at https://docs.subsquid.io/evm-indexing/configuration/initialization/#set-data-source
         rateLimit: 10
     })
@@ -31,10 +40,13 @@ export const processor = new EvmBatchProcessor()
         },
     })
     .setBlockRange({
-        from: 0,
+        from: networkConfig.fromBlock,
     })
     .addTransaction({
-        to: ['0x0000000000000000000000000000000000000000'],
+        to: [networkConfig.contractAddress.factory, networkConfig.contractAddress.router],
+    })
+    .addTransaction({
+        from: [networkConfig.contractAddress.factory, networkConfig.contractAddress.router],
     })
 
 export type Fields = EvmBatchProcessorFields<typeof processor>
