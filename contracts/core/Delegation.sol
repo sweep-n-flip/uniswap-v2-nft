2940e4056568f299ca9f254da0b27c43cca00239
Arthur RIbeiro <arthurmr13@hotmail.com>
2025-02-10 12:19:03 -0300

    feat: added apechain contracts and subgraph

--- contracts/core/Delegation.sol
+++ contracts/core/Delegation.sol
@@ -13,6 +13,7 @@ address constant DELEGATE_FACTORY = 0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac; 
 //address constant DELEGATE_FACTORY = 0xb08Bfed214ba87d5d5D07B7DA573010016C44488; // Kodiak (Berachain testnet)
 //address constant DELEGATE_FACTORY = 0xE578184bC88EB48485Bba23a37B5509578d2aE38; // IceSwap (StratoVM testnet)
 //address constant DELEGATE_FACTORY = 0x9945f4a1eC4C4FC74a70276CCf60b8b1B2DE1F4A; // - (Bitfinity testnet)
+//address constant DELEGATE_FACTORY = 0x7d8c6B58BA2d40FC6E34C25f9A488067Fe0D2dB4; // Camelot (Apechain)
 
 bytes constant DELEGATE_INIT_CODE_HASH = hex"e18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303"; // SushiSwap (all)
 //bytes constant DELEGATE_INIT_CODE_HASH = hex"57224589c67f3f30a6b0d7a1b54cf3153ab84563bc609ef41dfb34f8b2974d2d"; // PancakeSwap (Linea mainnet)
@@ -24,6 +25,7 @@ bytes constant DELEGATE_INIT_CODE_HASH = hex"e18a34eb0e04b04f7a0ac29a6e80748dca9
 //bytes constant DELEGATE_INIT_CODE_HASH = hex"0489c85ed300c1a9636d09ada5e1bea0e331f778464d45f24cb365c92cafbcb5"; // Kodiak (Berachain testnet)
 //bytes constant DELEGATE_INIT_CODE_HASH = hex"0437378fc27e93c612c5c385779bf540ca2064b54705e48c313aa216da380100"; // IceSwap (StratoVM testnet)
 //bytes constant DELEGATE_INIT_CODE_HASH = hex"5ae65da6662188c2e508cb6bcb59f05483a527fc27e586ef49c48f3ef9443469"; // - (Bitfinity testnet)
+//bytes constant DELEGATE_INIT_CODE_HASH = ""; // Camelot (Apechain)
 
 uint256 constant DELEGATE_NET_FEE = 9970; // SushiSwap
 //uint256 constant DELEGATE_NET_FEE = 9975; // PancakeSwap
